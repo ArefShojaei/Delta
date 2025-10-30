fea29a3fb6dbec1c8bbbe8b2905b3d912681b539
ArefShojaei <arefshojaei82@gmail.com>
2025-10-30 23:47:48 +0330

    REFACTOR: refactore Application component

--- src/Application/Application.php
+++ src/Application/Application.php
@@ -6,6 +6,7 @@ use Delta\Application\Contracts\ApplicationContract;
 use Delta\Application\Providers\ModuleLayerProvider;
 use Delta\Components\Routing\Router;
 use Delta\Components\Http\HttpBuilder;
+use Delta\Components\Http\Response;
 
 
 final class Application implements ApplicationContract
@@ -13,24 +14,32 @@ final class Application implements ApplicationContract
     private Router $router;
 
 
-    public function __construct(private string $app)
+    public function __construct(private readonly string $appModule)
     {
-
         $this->router = new Router;
 
-        $module = new ModuleLayerProvider($app, ["router" => $this->router]);
+        $module = new ModuleLayerProvider($appModule, ["router" => $this->router]);
     
         $module->process();
     }
 
     public function run(): void
     {
-        $httpBuilder = (new HttpBuilder)
-            ->setRequest()
-            ->setResponse()
-            ->setRouter($this->router)
-            ->build();
+        try {
+            $httpBuilder = (new HttpBuilder)
+                ->setRequest($_SERVER)
+                ->setResponse()
+                ->setRouter($this->router)
+                ->build();
 
-        $httpBuilder->listen();
+            $httpBuilder->listen();
+        } catch (\Exception $e) {
+            http_response_code(Response::HTTP_INTERNAL_SERVER_ERROR);
+
+            echo json_encode([
+                "code" => Response::HTTP_INTERNAL_SERVER_ERROR,
+                "error" => $e->getMessage(),
+            ]);
+        }
     }
 }
