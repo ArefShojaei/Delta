7700e4e544ae222d20d27e14b450f2176a720654
ArefShojaei <arefshojaei82@gmail.com>
2025-11-01 17:39:26 +0330

    ADD: add 'GetPrefix' & 'GetRoutes' methods

--- src/Application/Mixins/Layers/Module/HasModuleControllersDispatcherMixin.php
+++ src/Application/Mixins/Layers/Module/HasModuleControllersDispatcherMixin.php
@@ -2,8 +2,48 @@
 
 namespace Delta\Application\Mixins\Layers\Module;
 
+use Delta\Components\Routing\Attributes\Controller;
+use Delta\Components\Routing\Attributes\Route;
+use ReflectionAttribute;
+use ReflectionClass;
+use ReflectionMethod;
+
 
 trait HasModuleControllersDispatcherMixin
 {
-    private function dispatch(array $controllers) {}
+    private function getPrefix(ReflectionClass $reflection): ?string
+    {
+        $attributes = $reflection->getAttributes(Controller::class);
+        
+        $attribute = current($attributes)->newInstance();
+        
+        return $attribute->prefix;
+    }
+
+    private function getRoutes(ReflectionClass $reflection): array
+    {
+        $methods = $reflection->getMethods(ReflectionMethod::IS_PUBLIC);
+
+        $routes = [];
+
+        foreach ($methods as $method) {
+            $attributes = $method->getAttributes(Route::class, ReflectionAttribute::IS_INSTANCEOF);
+        
+            $attribute = current($attributes)->newInstance();
+
+            $routes[] = $attribute->path;
+        }
+
+        return $routes;
+    }
+
+    private function dispatch(array $controllers) {
+        foreach ($controllers as $controller) {
+            $controllerReflection = new ReflectionClass($controller);
+            
+            $this->getPrefix($controllerReflection);
+            
+            $this->getRoutes($controllerReflection);
+        }
+    }
 }
