8b865f47c000a52237f08e8401e57d82cd360f9c
ArefShojaei <arefshojaei82@gmail.com>
2025-11-03 17:49:33 +0330

    ADD: add Cookie component

---
+++ src/Components/Cookie/Cookie.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Delta\Components\Cookie;
+
+use Delta\Components\Cookie\Interfaces\Cookie as ICookie;
+
+
+final class Cookie implements ICookie {
+    public static function set(string $key, string $value): void
+    {
+        $_COOKIE[$key] = $value;
+    }
+
+    public static function get(string $key): ?string
+    {
+        return $_COOKIE[$key] ?? null;
+    }
+
+    public static function remove(string $key): bool
+    {
+        unset($_COOKIE[$key]);
+
+        return isset($_COOKIE[$key]);
+    }
+
+    public static function clean(): void
+    {
+        unset($_COOKIE);
+    }
+
+    public static function all(): array
+    {
+        return $_COOKIE;
+    }
+}
