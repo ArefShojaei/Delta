5624d9f80287b7e73a42a4ebb93bd4ae806b6852
ArefShojaei <arefshojaei82@gmail.com>
2025-11-03 17:49:21 +0330

    ADD: add Session component

---
+++ src/Components/Session/Interfaces/Session.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Delta\Components\Session\Interfaces;
+
+
+interface Session
+{
+    public static function set(string $key, string $value): void;
+    
+    public static function get(string $key): ?string;
+    
+    public static function remove(string $key): bool;
+    
+    public static function clean(): void;
+    
+    public static function all(): array;
+}
---
+++ src/Components/Session/Session.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Delta\Components\Session;
+
+use Delta\Components\Session\Interfaces\Session as ISession;
+
+
+final class Session implements ISession {
+    public static function set(string $key, string $value): void
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public static function get(string $key): ?string
+    {
+        return $_SESSION[$key] ?? null;
+    }
+
+    public static function remove(string $key): bool
+    {
+        unset($_SESSION[$key]);
+
+        return isset($_SESSION[$key]);
+    }
+
+    public static function clean(): void
+    {
+        unset($_SESSION);
+    }
+
+    public static function all(): array
+    {
+        return $_SESSION;
+    }
+}
