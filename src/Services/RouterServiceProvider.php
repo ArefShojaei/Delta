96327ebc57a6879a300275f9e4f6292757ff3171
ArefShojaei <=>
2025-11-05 20:05:33 +0330

    ADD: add Router service provider

---
+++ src/Services/RouterServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Delta\Services;
+
+use Delta\Bootstrap\Interfaces\ServiceProvider as IServiceProvider;
+use Delta\Components\{
+    Container\Container,
+    Routing\Router
+};
+
+
+final class RouterServiceProvider implements IServiceProvider
+{
+    public function register(Container $container): void
+    {
+        $container->bind(Router::class, Router::class);
+    }
+
+    public function boot(Container $container): void {}
+}
