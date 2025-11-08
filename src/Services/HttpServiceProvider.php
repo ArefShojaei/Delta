d3e34754b4a3c8bfcb1f660c21ff610cdfc1ace0
ArefShojaei <arefshojaei82@gmail.com>
2025-11-08 22:58:04 +0330

    UPDATE: update Http service provider

--- src/Services/HttpServiceProvider.php
+++ src/Services/HttpServiceProvider.php
@@ -4,7 +4,9 @@ namespace Delta\Services;
 
 use Delta\Bootstrap\Interfaces\ServiceProvider as IServiceProvider;
 use Delta\Components\Container\Container;
+use Delta\Components\Env\DotEnvEnvironment as Env;
 use Delta\Components\Http\{Http, Request, Response};
+use Delta\Components\Http\Builders\HttpBuilder;
 use Delta\Components\Routing\Router;
 
 
@@ -16,18 +18,18 @@ final class HttpServiceProvider implements IServiceProvider
 
         $container->bind(Response::class, Response::class);
 
-        $container->bind(Http::class, function (Container $container) {
-            return new Http(
-                request: $container->resolve(Request::class),
-                response: $container->resolve(Response::class),
-                router: $container->resolve(Router::class),
-            );
+        $container->bind(Http::class, function(Container $container) {
+            return (new HttpBuilder)
+                ->setRouter($container->resolve(Router::class))
+                ->setRequest($container->resolve(Request::class))
+                ->setResponse($container->resolve(Response::class))
+                ->build();
         });
     }
 
     public function boot(Container $container): void {
         $response = $container->resolve(Response::class);
 
-        $response->header("X-Powered-By", "Delta - PHP Framework");
+        $response->header("X-Powered-By", Env::get("APP_NAME", "Delta - PHP Framework"));
     }
 }
