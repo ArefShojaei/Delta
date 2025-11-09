3ac0c00ad4b5282b3900cc4bcb26c7b67b40e4d9
ArefShojaei <arefshojaei82@gmail.com>
2025-11-09 21:43:01 +0330

    UPDATE: update Route attribute to add route middlewares

--- src/Components/Routing/Attributes/Route.php
+++ src/Components/Routing/Attributes/Route.php
@@ -7,13 +7,18 @@ use Attribute;
 #[Attribute(Attribute::TARGET_METHOD)]
 abstract class Route
 {
-    public function __construct(public string $method, public string $path) {}
+    public function __construct(
+        public string $method = "",
+        public string $path = "",
+        public array $middlewares = [],
+    ) {}
 
     protected function toArray(): array
     {
         return [
             "method" => $this->method,
             "path" => $this->path,
+            "middlewares" => $this->middlewares
         ];
     }
 }
