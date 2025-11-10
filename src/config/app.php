08b5312bd7adf6e2ffba8ea86e78e4c4c8edd1f8
ArefShojaei <arefshojaei82@gmail.com>
2025-11-10 21:53:54 +0330

    UPDATE: update to rename Service to Providers as Bootstrap service providers

--- src/config/app.php
+++ src/config/app.php
@@ -10,8 +10,8 @@ return [
      * Application service providers as dependencies before bootstrapping the server
      */
     "providers" => [
-        Delta\Services\RouterServiceProvider::class,
-        Delta\Services\HttpServiceProvider::class,
+        Delta\Providers\RouterServiceProvider::class,
+        Delta\Providers\HttpServiceProvider::class,
     ],
 
     /**
