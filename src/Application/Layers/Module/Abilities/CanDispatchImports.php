b862e5a56500b071f9442900a550aa5063b5c8c7
ArefShojaei <arefshojaei82@gmail.com>
2025-11-13 23:44:01 +0330

    ADD: add to import subModules

--- src/Application/Layers/Module/Abilities/CanDispatchImports.php
+++ src/Application/Layers/Module/Abilities/CanDispatchImports.php
@@ -2,8 +2,16 @@
 
 namespace Delta\Application\Layers\Module\Abilities;
 
+use Delta\Components\Layer\LayerFactory;
+
 
 trait CanDispatchImports
 {
-    protected function dispatch(array $imports) {}
+    protected function dispatch(array $imports) {
+        foreach ($imports as $module) {
+            $subModuleLayer = LayerFactory::createModuleLayer($module, $this->container);
+
+            $subModuleLayer->process();
+        }
+    }
 }
