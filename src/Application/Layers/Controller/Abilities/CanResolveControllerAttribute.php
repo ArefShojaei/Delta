295a0c0b20491011efb175a8b2ce0715016669e9
ArefShojaei <arefshojaei82@gmail.com>
2025-11-15 20:42:57 +0330

    ADD: add to resolve Controller attribute

---
+++ src/Application/Layers/Controller/Abilities/CanResolveControllerAttribute.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Delta\Application\Layers\Controller\Abilities;
+
+use Delta\Components\Layer\Attributes\Controller;
+use ReflectionClass;
+
+
+trait CanResolveControllerAttribute
+{
+    private function getControllerAttribute(ReflectionClass $reflection):object
+    {
+        $attributes = $reflection->getAttributes(Controller::class);
+
+        return current($attributes)->newInstance();
+    }
+
+    private function getPrefix(ReflectionClass $reflection): ?string
+    {
+        $attribute = $this->getControllerAttribute($reflection);
+
+        return $attribute->prefix;
+    }
+
+    private function getRoutePrefixName(ReflectionClass $reflection): ?string
+    {
+        $attribute = $this->getControllerAttribute($reflection);
+
+        return $attribute->name;
+    }
+}
