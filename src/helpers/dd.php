486b42598d4e7020ca520d50a61c3ca2cbc6c184
ArefShojaei <arefshojaei82@gmail.com>
2025-11-15 20:39:43 +0330

    UPDATE: update to check the function before using that

--- src/helpers/dd.php
+++ src/helpers/dd.php
@@ -5,11 +5,13 @@
  *
  * @param mixed $input variable or data will be shown
  */
-function dd(mixed $input): void
-{
-    echo "<pre>";
-    is_array($input) ? print_r($input) : var_dump($input);
-    echo "</pre>";
+if (!function_exists("dd")) {
+    function dd(mixed $input): void
+    {
+        echo "<pre>";
+        is_array($input) ? print_r($input) : var_dump($input);
+        echo "</pre>";
 
-    exit;
+        exit();
+    }
 }
